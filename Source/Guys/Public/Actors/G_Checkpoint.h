2436c89ab69e2535e609b8c9d49612d904944625
Makyrios <lavreniuk.mykyta@lll.kpi.ua>
2024-01-17 12:01:46 +0200

    Checkpoints and player start

---
+++ Source/Guys/Public/Actors/G_Checkpoint.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "G_Checkpoint.generated.h"
+
+class UStaticMeshComponent;
+class UBoxComponent;
+
+UCLASS()
+class GUYS_API AG_Checkpoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AG_Checkpoint();
+
+protected:
+	virtual void BeginPlay() override;
+
+    UFUNCTION()
+	virtual void HandleCheckpointOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+    UPROPERTY(VisibleAnywhere, Category = "G|Components")
+    TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+    UPROPERTY(VisibleAnywhere, Category = "G|Components")
+    TObjectPtr<UBoxComponent> BoxCollider;
+
+    UPROPERTY(EditAnywhere, Category = "G|Checkpoint")
+    int Points = 10;
+
+
+private:
+    bool bTriggeredBefore = false;
+
+};
---
+++ Source/Guys/Public/Actors/G_Checkpoint_FinishLine.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/G_Checkpoint.h"
+#include "G_Checkpoint_FinishLine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUYS_API AG_Checkpoint_FinishLine : public AG_Checkpoint
+{
+	GENERATED_BODY()
+
+protected:
+    virtual void HandleCheckpointOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	
+};
